2c826e7860316eff52b886be10dbf407f7beec1d
Code-Gale <codewhiz001@gmail.com>
2024-12-23 19:55:20 +0100

    Software schema and model

---
+++ backend/src/models/Software.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const softwareSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    version: {
+        type: String,
+        required: true
+    },
+    filePath: {
+        type: String,
+        required: true
+    },
+    uploadedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Software = mongoose.model('Software', softwareSchema)
+module.exports = Software
