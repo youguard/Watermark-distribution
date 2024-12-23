bba00a82352264b0eba3aa4b69c9ecbf4258b4c5
Code-Gale <codewhiz001@gmail.com>
2024-12-23 19:55:34 +0100

    User schema and model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,42 @@
-A
+const mongoose = require('mongoose')
+const { deflateSync } = require('zlib')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String, 
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    isApproved: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    region: {
+        enum: ['Region-A', 'Region-B', 'Region-C', 'Region-D', 'Region-E', 'Region-F'],
+        default: 'Region A'
+    },
+    createdAt: {
+        type: Date,
+        defaul: Date.now
+    }
+})
+
+const User = mongoose.Model('User', userSchema)
+
+module.exports = User
