e532c01143239e8b76a59bfe9ada0fa5a8db83ec
Code-Gale <codewhiz001@gmail.com>
2024-12-23 19:54:57 +0100

    Notification schema and model

---
+++ backend/src/models/Notification.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const notificationSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Notification = mongoose.Model('Notification', notificationSchema)
+module.exports = Notification
