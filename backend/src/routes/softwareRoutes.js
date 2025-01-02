bd62d0ee64fcb18298611e8b5ed2953eff76b8b7
Code-Gale <codewhiz001@gmail.com>
2025-01-02 15:09:24 +0100

    software and users route

---
+++ backend/src/routes/softwareRoutes.js
@@ -0,0 +1,123 @@
+const { getSoftwares, getSoftwareById, downloadSoftware, uploadSoftware } = require('../controllers/softwareController')
+const router = require('express').Router()
+const protect = require('../middlewares/authMiddleware')
+
+
+/**
+ * @swagger
+ * /softwares:
+ *   get:
+ *     summary: Retrieve a list of softwares
+ *     responses:
+ *       200:
+ *         description: A list of softwares
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 softwares:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Software'
+ * /softwares/{id}:
+ *   get:
+ *     summary: Retrieve a software by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The software ID
+ *     responses:
+ *       200:
+ *         description: A software object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 software:
+ *                   $ref: '#/components/schemas/Software'
+ * /softwares/new:
+ *   post:
+ *     summary: Upload a new software
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               version:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       201:
+ *         description: Software uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                     description:
+ *                       type: string
+ *                     version:
+ *                       type: string
+ *                     fileUrl:
+ *                       type: string
+ *                     uploadedAt:
+ *                       type: string
+ *                       format: date-time
+ * /softwares/download:
+ *   get:
+ *     summary: Download a software
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Software download URL
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 downloadUrl:
+ *                   type: string
+ */
+router.get('/softwares', getSoftwares)
+router.get('/softwares/:id', getSoftwareById)
+router.post('/softwares/new', protect, uploadSoftware)
+router.get('/softwares/download', protect, downloadSoftware)
+
+module.exports = router
---
+++ backend/src/routes/userController.js
@@ -0,0 +1,99 @@
+const { getUsers, getUserById, approveUser, revokeUser, updateUser, deleteUser} = require('../controllers/userController')
+const protect = require('../middlewares/authMiddleware')
+const router = require('express').Router()
+
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ * /users/{id}:
+ *   get:
+ *     summary: Retrieve a single user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: A single user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *   patch:
+ *     summary: Approve a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: User approved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *   put:
+ *     summary: Update a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: User updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *   delete:
+ *     summary: Delete a user by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: User deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
+router.get('/users', getUsers)
+router.get('/users/:id', getUserById)
+router.patch('/users/:id', approveUser)
+router.patch('/users/:id', revokeUser)
+router.put('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser)
+
+module.exports = router
