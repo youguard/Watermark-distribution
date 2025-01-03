3219bf5d058aaf184106d727ff4f8bac95327a80
Code-Gale <codewhiz001@gmail.com>
2025-01-03 20:11:22 +0100

    update

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -72,7 +72,7 @@ const login = async(req, res) => {
         }
 
         if(!user){
-            res.status(400).json({message: 'User Not Found...'})
+            return res.status(400).json({message: 'User Not Found...'})
         }
         const matchedPassword = await bcrypt.compare(password, user.password)
         if(!matchedPassword){
