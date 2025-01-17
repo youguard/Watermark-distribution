80c8f67a48f1bfeccf8403cdbc77efcc127a61e0
Code-Gale <codewhiz001@gmail.com>
2025-01-17 09:46:53 +0100

    fixes

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -383,7 +383,7 @@ const resetPassword = async (req, res) => {
 
 const changePassword = async (req, res) => {
     try{
-        const userId = req.user_id
+        const userId = req.user._id
         const user = await User.findById(userId)
         if(!user){
             return res.status(400).json({message: 'User not authenticated'})
