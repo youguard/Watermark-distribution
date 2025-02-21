d9c3d8caae2affccdd0e973f05f532f98a84fa39
youguard <yoosekwang7@gmail.com>
2025-02-21 12:52:40 +0900

    back Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -11,15 +11,6 @@ export default defineNuxtConfig({
   router: {
     middleware: ['auth'],
   },
+  
   css: ['~/assets/css/main.css', 'vue3-toastify/dist/index.css'],
-
-  // 🔹 Google Search Console & 네이버 웹마스터 도구 인증 태그 추가
-  app: {
-    head: {
-      meta: [
-        { name: "google-site-verification", content: "gdclOcaoXxkxln1EP7TK3QsdwG4rMZr-rMnUxtp6LGQ" },
-        { name: "naver-site-verification", content: "b80e90d89793733822fd10860efc3c2276541563" }
-      ]
-    }
-  }
-});
+})
